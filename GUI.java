2e757cdec27f1ac73a6c1fd60eb9acd8162aa67f
Owen Gray <71647214+grayowen135@users.noreply.github.com>
2021-11-26 15:51:40 +0000

    added a method and fixed loop

---
+++ GUI.java
@@ -0,0 +1,15 @@
+// import javax.swing.*;
+
+//public class GUI extends JFrame {
+
+  // TRYING to start a gui //
+  //public static void GUI () {
+    //JFrame frame = new JFrame ("Iventory");
+    //frame.setDeaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+    //frame.setSize(500,300);
+    //frame.setVisible(true);
+    //String[] coloum_names = {"Item Name", "Price", "Price With Tax", "Quantity", "Product Number"};
+    //Object[][] table_data = {};
+    //JTable table = new JTable(table_data, coloum_names);
+  //}
+//}
--- Main.java
+++ Main.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 class Main {
 
-  // TRYING to start a gui //
-  public static void GUI(String[] args) {
-    JFrame frame = new JFrame ("Iventory");
+  public static void myTaxMethod(Object [][] array_all, byte item_numb) {
+    for (int i = 0; i < item_numb; i++) {
+    array_all[item_numb][4] = ((Float.parseFloat(String.valueOf(array_all[2][i]))*1.13f));
+    }
   }
 
   // Initializing Lists and variables // 
   public static void main(String[] args) throws IOException {
     Scanner input = new Scanner(System.in);
-    byte i = 0;
+    byte i = 0; byte j = 0;
     System.out.println("Enter how many items you want.");
     byte item_numb = input.nextByte();
-    ArrayList<String> list_name = new ArrayList<String>();
-    ArrayList<Float> list_price = new ArrayList<Float>();
-    ArrayList<Byte> list_quantity = new ArrayList<Byte>();
+    //ArrayList<String> list_name = new ArrayList<String>();
+    //ArrayList<Float> list_price = new ArrayList<Float>();
+    //ArrayList<Byte> list_quantity = new ArrayList<Byte>();
 
     // Adding The File //
     File saved_inventory = new File("Saved_Inventory.txt");
@@ -30,22 +31,31 @@ class Main {
     System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
     char choice_select = input.next().charAt(0);
     char choiceSelectCaps = Character.toUpperCase(choice_select);
+
+    Object [][] array_all= new Object [item_numb][5];
     while (choiceSelectCaps != 'D') {
+      //new GUI(item_numb, array_all);
 
       // Adding Items //
       if (choiceSelectCaps == 'A') {
-        for (i = 0; i < item_numb; i++) {
+        for (i = 0; i < item_numb; i++){
           System.out.println("Enter the name of item " + (i + 1));
           input.nextLine();
-          String add_name = input.nextLine();
-          list_name.add(add_name);
+          array_all[1][i] = input.nextLine();
           System.out.println("Enter the price of item " + (i + 1));
-          float add_price = input.nextFloat();
-          list_price.add(add_price);
+          array_all[1][i] = input.nextFloat();
           System.out.println("Enter the quantity of item " + (i + 1));
-          byte add_quantity = input.nextByte();
-          list_quantity.add(add_quantity);
-          write_file.write(list_name.get(i) + ": $" + list_price.get(i) + ", there are " + list_quantity.get(i) + " in inventory.\n");
+          array_all[1][i] = input.nextByte();
+        }
+
+        // Calling Tax Method //
+        myTaxMethod(array_all, item_numb);
+
+        // Writing to the File //     
+        for (i = 0; i < item_numb; i++) {
+          for (j = 0; j < 5; j++) {
+            write_file.write(array_all[i][j]+"\n");
+          }
         }
         write_file.close();
         System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
@@ -53,19 +63,8 @@ class Main {
         choiceSelectCaps = Character.toUpperCase(choice_select);
       } 
 
-      // Show w/ Tax //
-      else if (choiceSelectCaps == 'B') {
-        System.out.println("Which item would you like to calculate tax for?");
-        byte itemToTax = input.nextByte();
-        float item_taxed = ((list_price.get(itemToTax))*1.13f);
-        System.out.println(item_taxed);
-        System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
-        choice_select = input.next().charAt(0);
-        choiceSelectCaps = Character.toUpperCase(choice_select);
-      }
-
       // Show Inventory //
-      else if (choiceSelectCaps == 'C') {
+      else if (choiceSelectCaps == 'B') {
         Scanner myReader = new Scanner(saved_inventory);
         while (myReader.hasNextLine()) {
           String file_out = myReader.nextLine();
