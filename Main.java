15b729a38cb123d00e3941f4d17d61fe5b1156cf
Owen Gray <71647214+grayowen135@users.noreply.github.com>
2021-12-05 06:38:30 +0000

    YES BOI  I AM GOLD 3 IN VALORANT

--- Main.java
+++ Main.java
@@ -5,74 +5,129 @@ import java.io.FileWriter;
 
 class Main {
 
-  public static void myTaxMethod(Object [][] array_all, byte item_numb) {
-    for (int i = 0; i < item_numb; i++) {
-    array_all[i][3] = (Float.parseFloat(String.valueOf(array_all[i][1]))*1.13f);
-    }
+  // Tax Method //
+  public static void myTaxMethod(Object[][] array_all, int itemnumb) {
+    array_all[itemnumb][3] = ((float) array_all[itemnumb][1] * 1.13f);
   }
 
-  // Initializing Lists and variables // 
+  // Initializing Lists and variables //
   public static void main(String[] args) throws IOException {
     Scanner input = new Scanner(System.in);
-    byte i = 0; byte j = 0; boolean bool = false; byte item_numb = 0;
-
-    // Making sure item number is greater than or equal to 5 //
-    while (bool == false) {
-    System.out.println("Enter how many items you want.");
-    item_numb = input.nextByte();
-      try {
-        if (item_numb < 5) {
-          throw new ArithmeticException();
-        }
-        else {
-          bool = true;
-        }
-      }
-      catch (Exception e) {
-        System.out.println("5 items minimum, please enter a number bigger or equal to 5");
-      }
-    }
+    boolean bool_B = false;
+    boolean bool_C = false;
+    int itemnumb = 5;
+    Object[][] array_all = new Object[itemnumb][5];
 
     // Adding The File //
     File saved_inventory = new File("Saved_Inventory.txt");
     saved_inventory.createNewFile();
     FileWriter write_file = new FileWriter("Saved_Inventory.txt");
 
+    for (byte i = 0; i < 5; i++) {
+      System.out.println("Enter the name of item " + (i + 1));
+      array_all[i][0] = input.nextLine();
+      if (i != 0) {
+        array_all[i][0] = input.nextLine();
+      }
+      System.out.println("Enter the price of item " + (i + 1));
+      array_all[i][1] = input.nextFloat();
+      System.out.println("Enter the quantity of the item " + (i + 1));
+      array_all[i][2] = input.nextByte();
+      while (bool_B == false) {
+        System.out.println("Enter the 10 digit product number for the item " + (i + 1));
+        array_all[i][4] = input.nextLong();
+        try {
+          if ((Long.toString((long) array_all[i][4]).length() != 10)) {
+            throw new ArithmeticException();
+          } else {
+            bool_B = true;
+          }
+        } catch (Exception e) {
+          System.out.println("The product number must be 10 digits, please enter a valid number.");
+        }
+      }
+      bool_B = false;
+
+      // Calling Tax Method //
+      myTaxMethod(array_all, i);
+
+      // Writing to the File //
+        for (byte j = 0; j < 5; j++) {
+          write_file.write(array_all[i][j] + "\n");
+        }
+        if (i == 4) {
+          write_file.close();
+        }
+    }
     // Entering the Loop //
-    System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
+    System.out.println("What would you like to do? \nA. Add an Item \nB. Show current inventory \nC. Exit");
     char choice_select = input.next().charAt(0);
     char choiceSelectCaps = Character.toUpperCase(choice_select);
-
-    Object [][] array_all= new Object [item_numb][4];
-    while (choiceSelectCaps != 'D') {
-      //new GUI(item_numb, array_all);
-
-      // Adding Items //
-      if (choiceSelectCaps == 'A') {
-        for (i = 0; i < item_numb; i++){
-          System.out.println("Enter the name of item " + (i + 1));
-          input.nextLine();
-          array_all[i][0] = input.nextLine();
-          System.out.println("Enter the price of item " + (i + 1));
-          array_all[i][1] = input.nextFloat();
-          System.out.println("Enter the quantity of item " + (i + 1));
-          array_all[i][2] = input.nextByte();
+    while (bool_C == false) {
+      try {
+        if (choiceSelectCaps != 'A' && choiceSelectCaps != 'B' && choiceSelectCaps != 'C') {
+          throw new ArithmeticException();
+        } else {
+          bool_C = true;
         }
+      } catch (Exception e) {
+        System.out.println("Not an availible option.");
+      }
+    }
+
+    while (choiceSelectCaps != 'C') {
+      // new GUI(itemnumb, array_all);
+
+      // Adding an Item //
+      if (choiceSelectCaps == 'A') {
+        itemnumb++;
+        byte l = 0;
+        System.out.println("Enter the name of the item");
+        input.nextLine();
+        array_all[l + 5][0] = input.nextLine();
+        System.out.println("Enter the price of the item");
+        array_all[l + 5][1] = input.nextFloat();
+        System.out.println("Enter the quantity of the item");
+        array_all[l + 5][2] = input.nextByte();
+        while (bool_B == false) {
+          System.out.println("Enter the 10 digit product number for the item");
+          array_all[l + 5][4] = input.nextLong();
+          try {
+            if ((Long.toString((long) array_all[l + 5][4]).length() != 10)) {
+              throw new ArithmeticException();
+            } else {
+              bool_B = true;
+            }
+          } catch (Exception e) {
+            System.out.println("The product number must be 10 digits, please enter a valid number.");
+          }
+        }
+        bool_B = false;
 
         // Calling Tax Method //
-        myTaxMethod(array_all, item_numb);
+        myTaxMethod(array_all, itemnumb);
 
-        // Writing to the File //     
-        for (i = 0; i < item_numb; i++) {
-          for (j = 0; j < 4; j++) {
-            write_file.write(array_all[i][j]+" ");
-            System.out.println("\t");
+        // Writing to the File //
+        for (byte i = 0; i < itemnumb; i++) {
+          for (byte j = 0; j < 5; j++) {
+            write_file.write(array_all[i][j] + "\n");
           }
         }
         write_file.close();
-        System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
+        System.out.println("What would you like to do? \nA. Add an Item \nB. Show current inventory \nC. Exit");
         choice_select = input.next().charAt(0);
-        choiceSelectCaps = Character.toUpperCase(choice_select); 
+        choiceSelectCaps = Character.toUpperCase(choice_select);
+        while (bool_C == false) {
+          try {
+            if (choiceSelectCaps != 'A' && choiceSelectCaps != 'B' && choiceSelectCaps != 'C') {
+              throw new ArithmeticException();
+            } else {
+              bool_C = true;
+            }
+          } catch (Exception e) {
+            System.out.println("Not an availible option.");
+          }
+        }
       }
 
       // Show Inventory //
@@ -83,9 +138,20 @@ class Main {
           System.out.println(file_out);
         }
         myReader.close();
-        System.out.println("What would you like to do? \nA. Add Items \nB. Show current inventory \nC. Exit");
+        System.out.println("What would you like to do? \nA. Add an Item \nB. Show current inventory \nC. Exit");
         choice_select = input.next().charAt(0);
         choiceSelectCaps = Character.toUpperCase(choice_select);
+        while (bool_C == false) {
+          try {
+            if (choiceSelectCaps != 'A' && choiceSelectCaps != 'B' && choiceSelectCaps != 'C') {
+              throw new ArithmeticException();
+            } else {
+              bool_C = true;
+            }
+          } catch (Exception e) {
+            System.out.println("Not an availible option.");
+          }
+        }
       }
     }
   }
